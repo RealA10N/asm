29d2d5fbf33e5fd00e33fc752e3d49a463c4830e
Alon Krymgand <me@alon.kr>
2023-10-19 14:37:08 +0300

    Added fopen

--- brainfuck/bf.asm
+++ brainfuck/bf.asm
@@ -5,10 +5,32 @@ main:
         cmp edi, 2
         js .no_source_file_provided
 
+        add rsi, 8      ; rsi = code filename string
+
+        extern fopen ; open source code file
+        mov rdi, rsi
+        mov rsi, read_only
+        sub rsp, 8
+        call fopen
+        add rsp, 8
+
+        ; descriptor now in rax
+        cmp rax, 0
+        jz .failed_to_open_source_file
+
+
+.failed_to_open_source_file:
+        mov rdi, failed_to_open_file_msg
+        jmp exit_and_report_error
 
 .no_source_file_provided:
-        extern exit, puts
         mov rdi, no_source_file_msg
+        jmp exit_and_report_error
+
+exit_and_report_error:
+        ; recieve error message in rdi,
+        ; print and exit with code 1
+        extern exit, puts
         sub rsp, 8
         call puts
         add rsp, 8
@@ -17,7 +39,23 @@ main:
         sub rsp, 8
         call exit
 
+
+section .bss
+        code resb 1024*1024
+        stack resb 1024*1024
+
 section .data
+
+        data times 2*1024*1024 db 0
+        dend db 0
+        dptr dq data+(1024*1024)
+
+        sptr dq stack
+        send dq stack + (1024*1024)
 
 no_source_file_msg:
         db 'error: no input file', 10, 0
+failed_to_open_file_msg:
+        db 'error: failed to open source file', 10, 0
+read_only:
+        db 'r', 0
